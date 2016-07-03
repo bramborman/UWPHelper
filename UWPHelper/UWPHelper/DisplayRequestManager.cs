47410b262d9e2f4b4dd9ca9ceca19de023519e0f
Marian Dolinský <mariandolinsky@outlook.com>
2016-07-03 23:09:02 +0200

    Added DisplayRequestManager

---
+++ UWPHelper/UWPHelper/DisplayRequestManager.cs
@@ -0,0 +1,37 @@
+﻿using Windows.System.Display;
+
+namespace UWPHelper
+{
+    public class DisplayRequestManager
+    {
+        DisplayRequest displayRequest;
+
+        bool _isActive;
+
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set
+            {
+                if (_isActive != value)
+                {
+                    _isActive = value;
+
+                    if (_isActive)
+                    {
+                        if (displayRequest == null)
+                        {
+                            displayRequest = new DisplayRequest();
+                        }
+
+                        displayRequest.RequestActive();
+                    }
+                    else
+                    {
+                        displayRequest.RequestRelease();
+                    }
+                }
+            }
+        }
+    }
+}
