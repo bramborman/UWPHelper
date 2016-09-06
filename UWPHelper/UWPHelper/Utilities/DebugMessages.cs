c4161fb900b1b0ba2a44593741123cd2c0bff4d5
Marian Dolinský <mariandolinsky@outlook.com>
2016-09-06 19:51:41 +0200

    Improved DebugMessages.OperationInfo method, added new Test app icons

--- UWPHelper/UWPHelper/Utilities/DebugMessages.cs
+++ UWPHelper/UWPHelper/Utilities/DebugMessages.cs
@@ -8,7 +8,7 @@ namespace UWPHelper.Utilities
         [Conditional("DEBUG")]
         public static void OperationInfo(string name, string operation, bool success)
         {
-            Debug.WriteLine($"{name} {operation} {(success ? "succeeded" : "failed")} at {DateTime.Now.Hour:D2}:{DateTime.Now.Minute:D2}:{DateTime.Now.Second:D2}");
+            Debug.WriteLine($"{name} {operation} {(success ? "succeeded" : "failed")} at {DateTime.Now:hh:mm:ss}");
         }
     }
 }
