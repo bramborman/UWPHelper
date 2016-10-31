29f997abd50cf2fd206fd37219c400cdabcddef5
Marian Dolinský <mariandolinsky@outlook.com>
2016-10-31 18:06:43 +0100

    ResourceLoader renamed to ResourceLoaderConverter, added newlines at ends of files

--- UWPHelper/UWPHelper/Converters/ResourceLoaderConverter.cs
+++ UWPHelper/UWPHelper/Converters/ResourceLoaderConverter.cs
@@ -3,7 +3,7 @@ using Windows.UI.Xaml.Data;
 
 namespace UWPHelper.Converters
 {
-    public sealed class ResourceLoader : IValueConverter
+    public sealed class ResourceLoaderConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
