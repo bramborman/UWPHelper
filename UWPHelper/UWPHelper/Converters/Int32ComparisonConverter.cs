2baf09848c8869ad34010642c029f8766b8fc252
Marian Dolinský <mariandolinsky@outlook.com>
2016-12-01 21:30:58 +0100

    Added Int32ComparisonConverter and KeyboardHelper

---
+++ UWPHelper/UWPHelper/Converters/Int32ComparisonConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace UWPHelper.Converters
+{
+    public sealed class Int32ComparisonConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string[] parameters = ((string)parameter).Split('_');
+
+            if (parameters.Length != 2)
+            {
+                throw new ArgumentException($"Incorrect formatting of {nameof(parameter)}.", nameof(parameter));
+            }
+
+            switch (parameters[0])
+            {
+                case "Equals":          return System.Convert.ToInt32(value) == (int)double.Parse(parameters[1]);
+                case "Greater":         return System.Convert.ToInt32(value)  > (int)double.Parse(parameters[1]);
+                case "Lower":           return System.Convert.ToInt32(value)  < (int)double.Parse(parameters[1]);
+                case "GreaterOrEquals": return System.Convert.ToInt32(value) >= (int)double.Parse(parameters[1]);
+                case "LowerOrEquals":   return System.Convert.ToInt32(value) <= (int)double.Parse(parameters[1]);
+                default:                throw new ArgumentException($"Incorrect formatting of {nameof(parameter)}.", nameof(parameter));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UWPHelper/UWPHelper/Utilities/KeyboardHelper.cs
@@ -0,0 +1,19 @@
+﻿using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace UWPHelper.Utilities
+{
+    public static class KeyboardHelper
+    {
+        public static bool IsDown(VirtualKey key)
+        {
+            return IsInState(key, CoreVirtualKeyStates.Down);
+        }
+
+        public static bool IsInState(VirtualKey key, CoreVirtualKeyStates state)
+        {
+            return Window.Current.CoreWindow.GetKeyState(key).HasFlag(state);
+        }
+    }
+}
