7aa0dde260622f74a30bdcc8497eb1494e3730cd
Marian Dolinský <mariandolinsky@outlook.com>
2016-12-14 17:17:42 +0100

    Added NumberTextBox, added some capabilities to AdvancedTextBox, minor changes

--- UWPHelper/UWPHelper/UI/AdvancedTextBox.cs
+++ UWPHelper/UWPHelper/UI/AdvancedTextBox.cs
@@ -7,29 +7,79 @@ namespace UWPHelper.UI
 {
     public class AdvancedTextBox : TextBox
     {
+        public static readonly DependencyProperty SelectAllOnGotFocusProperty        = DependencyProperty.Register(nameof(SelectAllOnGotFocus), typeof(bool), typeof(AdvancedTextBox), null);
+        public static readonly DependencyProperty IsTextFallbackValueEnabledProperty = DependencyProperty.Register(nameof(IsTextFallbackValueEnabled), typeof(bool), typeof(AdvancedTextBox), null);
+        public static readonly DependencyProperty TextFallbackValueProperty          = DependencyProperty.Register(nameof(TextFallbackValue), typeof(string), typeof(AdvancedTextBox), null);
+
+        public bool SelectAllOnGotFocus
+        {
+            get { return (bool)GetValue(SelectAllOnGotFocusProperty); }
+            set { SetValue(SelectAllOnGotFocusProperty, value); }
+        }
+        public bool IsTextFallbackValueEnabled
+        {
+            get { return (bool)GetValue(IsTextFallbackValueEnabledProperty); }
+            set { SetValue(IsTextFallbackValueEnabledProperty, value); }
+        }
+        public string TextFallbackValue
+        {
+            get { return (string)GetValue(TextFallbackValueProperty); }
+            set { SetValue(TextFallbackValueProperty, value); }
+        }
+
         public event RoutedEventHandler SubmitKeyDown;
         public event RoutedEventHandler SubmitKeyUp;
         public event RoutedEventHandler EscapeKeyDown;
         public event RoutedEventHandler EscapeKeyUp;
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            if (SelectAllOnGotFocus)
+            {
+                SelectAll();
+            }
+
+            base.OnGotFocus(e);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            if (IsTextFallbackValueEnabled && string.IsNullOrWhiteSpace(Text))
+            {
+                Text = TextFallbackValue;
+            }
+
+            base.OnLostFocus(e);
+        }
 
         protected override void OnKeyDown(KeyRoutedEventArgs e)
         {
             switch (e.Key)
             {
                 case VirtualKey.Enter:
-                    e.Handled = true;
-                    SubmitKeyDown?.Invoke(this, new RoutedEventArgs());
+                    RoutedEventHandler submitKeyDownHandler = SubmitKeyDown;
+
+                    if (submitKeyDownHandler != null)
+                    {
+                        e.Handled = true;
+                        submitKeyDownHandler.Invoke(this, new RoutedEventArgs());
+                    }
+
                     break;
 
                 case VirtualKey.Escape:
-                    e.Handled = true;
-                    EscapeKeyDown?.Invoke(this, new RoutedEventArgs());
-                    break;
-                
-                default:
-                    base.OnKeyDown(e);
+                    RoutedEventHandler escapeKeyDownHandler = EscapeKeyDown;
+
+                    if (escapeKeyDownHandler != null)
+                    {
+                        e.Handled = true;
+                        escapeKeyDownHandler.Invoke(this, new RoutedEventArgs());
+                    }
+
                     break;
             }
+
+            base.OnKeyDown(e);
         }
 
         protected override void OnKeyUp(KeyRoutedEventArgs e)
@@ -37,19 +87,29 @@ namespace UWPHelper.UI
             switch (e.Key)
             {
                 case VirtualKey.Enter:
-                    e.Handled = true;
-                    SubmitKeyUp?.Invoke(this, new RoutedEventArgs());
+                    RoutedEventHandler submitKeyUpHandler = SubmitKeyUp;
+
+                    if (submitKeyUpHandler != null)
+                    {
+                        e.Handled = true;
+                        submitKeyUpHandler.Invoke(this, new RoutedEventArgs());
+                    }
+
                     break;
 
                 case VirtualKey.Escape:
-                    e.Handled = true;
-                    EscapeKeyUp?.Invoke(this, new RoutedEventArgs());
-                    break;
+                    RoutedEventHandler escapeKeyUpHandler = EscapeKeyUp;
 
-                default:
-                    base.OnKeyUp(e);
+                    if (escapeKeyUpHandler != null)
+                    {
+                        e.Handled = true;
+                        escapeKeyUpHandler.Invoke(this, new RoutedEventArgs());
+                    }
+
                     break;
             }
+
+            base.OnKeyUp(e);
         }
     }
 }
---
+++ UWPHelper/UWPHelper/UI/NumberTextBox.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace UWPHelper.UI
+{
+    public class NumberTextBox : AdvancedTextBox
+    {
+        public NumberTextBox()
+        {
+            InputScope = new InputScope();
+            InputScope.Names.Add(new InputScopeName { NameValue = InputScopeNameValue.Number });
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            TextChanging += TextValidation;
+            base.OnGotFocus(e);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            TextChanging -= TextValidation;
+            base.OnLostFocus(e);
+        }
+
+        private void TextValidation(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            int cursorPosition = SelectionStart;
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                if (!char.IsDigit(Text[i]))
+                {
+                    Text = Text.Remove(i, 1);
+                }
+            }
+
+            SelectionStart = cursorPosition;
+        }
+    }
+}
