935851b5598bfb4b8a9d6ac8b636ad3de38a5e27
Marian Dolinský <mariandolinsky@outlook.com>
2016-12-17 19:15:02 +0100

    Added UWPHelper license to AboutApp

--- UWPHelper/UWPHelper/UI/AboutApp.xaml.cs
+++ UWPHelper/UWPHelper/UI/AboutApp.xaml.cs
@@ -15,6 +15,17 @@ namespace UWPHelper.UI
 {
     public sealed partial class AboutApp : UserControl
     {
+        #region Licenses
+        private const string UWPHELPER_LICENSE =
+@"MIT License
+
+Copyright (c) 2016 Marian Dolinský
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
         private const string JSON_NET_LICENSE =
 @"The MIT License (MIT)
 
@@ -25,12 +36,13 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+        #endregion
 
         public static readonly DependencyProperty AppStoreIdProperty             = DependencyProperty.Register(nameof(AppStoreId), typeof(string), typeof(AboutApp), null);
         public static readonly DependencyProperty AppUriProperty                 = DependencyProperty.Register(nameof(AppUri), typeof(string), typeof(AboutApp), null);
         public static readonly DependencyProperty AppLogoPathProperty            = DependencyProperty.Register(nameof(AppLogoPath), typeof(string), typeof(AboutApp), new PropertyMetadata(@"ms-appx:Assets/AppLogo.png"));
         public static readonly DependencyProperty AppDeveloperMailProperty       = DependencyProperty.Register(nameof(AppDeveloperMail), typeof(string), typeof(AboutApp), null);
-        public static readonly DependencyProperty ThirdPartySoftwareInfoProperty = DependencyProperty.Register(nameof(ThirdPartySoftwareInfo), typeof(List<ThirdPartySoftwareInfo>), typeof(AboutApp), new PropertyMetadata(new List<ThirdPartySoftwareInfo> { new ThirdPartySoftwareInfo { Name = "Json.NET", License = JSON_NET_LICENSE } }));
+        public static readonly DependencyProperty ThirdPartySoftwareInfoProperty = DependencyProperty.Register(nameof(ThirdPartySoftwareInfo), typeof(List<ThirdPartySoftwareInfo>), typeof(AboutApp), new PropertyMetadata(new List<ThirdPartySoftwareInfo> { new ThirdPartySoftwareInfo { Name = "UWPHelper", License = UWPHELPER_LICENSE }, new ThirdPartySoftwareInfo { Name = "Json.NET", License = JSON_NET_LICENSE } }));
         public static readonly DependencyProperty IsGitHubLinkEnabledProperty    = DependencyProperty.Register(nameof(IsGitHubLinkEnabled), typeof(bool), typeof(AboutApp), null);
         public static readonly DependencyProperty GitHubProjectNameProperty      = DependencyProperty.Register(nameof(GitHubProjectName), typeof(string), typeof(AboutApp), null);
         public static readonly DependencyProperty GitHubLinkUrlProperty          = DependencyProperty.Register(nameof(GitHubLinkUrl), typeof(string), typeof(AboutApp), null);
