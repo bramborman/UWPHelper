9625d5443aecbb1571ee3edf430d0bcae2bbccd8
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-05 00:37:53 +0100

    Added ViewHelper, DebugHelper replaced with TraceHelper, since it will not work since UWPHelper library is compiled in Release mode

--- UWPHelper/Utilities/TraceHelper.cs
+++ UWPHelper/Utilities/TraceHelper.cs
@@ -3,9 +3,9 @@ using System.Diagnostics;
 
 namespace UWPHelper.Utilities
 {
-    public static class DebugHelper
+    public static class TraceHelper
     {
-        [Conditional("DEBUG")]
+        [Conditional("TRACE")]
         public static void OperationInfo(string objectName, string operationName, bool success)
         {
             Debug.WriteLine($"{objectName} {operationName} {(success ? "succeeded" : "failed")} at {DateTime.Now:HH:mm:ss}");
---
+++ UWPHelper/Utilities/ViewHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+
+namespace UWPHelper.Utilities
+{
+    internal static class ViewHelper
+    {
+        internal static CoreWindow GetCurrentCoreWindow()
+        {
+            return CoreWindow.GetForCurrentThread() ?? throw new Exception($"Unable to get current {nameof(CoreWindow)}. Please make sure you are calling this method on an UI thread.");
+        }
+
+        internal static int GetCurrentViewId()
+        {
+            return ApplicationView.GetApplicationViewIdForWindow(GetCurrentCoreWindow());
+        }
+    }
+}
