605b5857322ec07905bdf2bffa3e03ef80eb4660
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-17 16:35:08 +0100

    Added SharedAssembleInfo file, updated AssemblyInfo files in each project

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Marian Dolinský")]
+[assembly: AssemblyCopyright("© 2017 Marian Dolinský")]
+[assembly: AssemblyVersion("1.5.9.2")]
+[assembly: AssemblyFileVersion("1.5.9.2")]
