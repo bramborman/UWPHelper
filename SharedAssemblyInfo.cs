228949038bea229a3fee0ba0b65b72a0cecaeb3e
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-17 19:22:29 +0100

    Bumped version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("Marian Dolinský")]
 [assembly: AssemblyCopyright("© 2017 Marian Dolinský")]
-[assembly: AssemblyVersion("1.5.9.2")]
-[assembly: AssemblyFileVersion("1.5.9.2")]
+[assembly: AssemblyVersion("1.6.10.2")]
+[assembly: AssemblyFileVersion("1.6.10.2")]
