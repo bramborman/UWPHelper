11fed53ea66b411843ecb512f758c2bfc3366082
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-18 20:42:36 +0100

    Simplified SharedAssemblyInfo

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,13 +5,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© 2017 Marian Dolinský")]
 [assembly: AssemblyVersion("1.6.11.3")]
 [assembly: AssemblyFileVersion("1.6.11.3")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-
 [assembly: ComVisible(false)]
