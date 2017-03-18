0da92bc9fcaf212050e70fc08a86a8998197438c
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-18 20:31:29 +0100

    More simplitications using SharedAssemblyInfo

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,6 +1,17 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Marian Dolinský")]
 [assembly: AssemblyCopyright("© 2017 Marian Dolinský")]
 [assembly: AssemblyVersion("1.6.11.3")]
 [assembly: AssemblyFileVersion("1.6.11.3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
--- UWPHelper.SampleApp/Properties/AssemblyInfo.cs
+++ UWPHelper.SampleApp/Properties/AssemblyInfo.cs
@@ -1,10 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("UWPHelper Sample App")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("UWPHelper Sample App")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
