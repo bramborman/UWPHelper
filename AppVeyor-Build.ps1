9095c3606c051f153cc6e67909ab6294450fbcb0
Marian Dolinský <mariandolinsky@outlook.com>
2017-09-29 10:51:48 +0200

    Testing improved build script

--- AppVeyor-Build.ps1
+++ AppVeyor-Build.ps1
@@ -5,31 +5,10 @@ Write-Host   "=============================="
 Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Set-BuildVersion.ps1"
 .\Set-BuildVersion.ps1
 
-# Use this script after UWPHelper went out of preview
-# Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Set-PureBuildVersion.ps1"
-# .\Set-PureBuildVersion.ps1
+Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Set-PureBuildVersion.ps1"
+.\Set-PureBuildVersion.ps1
 
-$buildVersion = $env:APPVEYOR_BUILD_VERSION
-
-# Check whether this is commit in branch 'master' and not just PR to the branch
-if (($env:APPVEYOR_REPO_BRANCH -eq "master") -and ($env:APPVEYOR_PULL_REQUEST_TITLE -eq $null) -and ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null))
-{
-	$newVersion	= $buildVersion.Split("-") | Select-Object -first 1
-	$newVersion = "$newVersion-preview"
-	$message    = "Build version changed from '$buildVersion' to '$newVersion'."
-
-	$buildVersion = $newVersion
-	Update-AppveyorBuild -Version $buildVersion
-	# Set the environment variable explicitly so it will be preserved to deployments (specifically GitHub Releases)
-	Set-AppveyorBuildVariable "APPVEYOR_BUILD_VERSION" $buildVersion
-
-	Add-AppveyorMessage $message
-	Write-Host $message
-}
-
-# Have to use different version because of a bug in 4.0.0
-Start-FileDownload "https://dist.nuget.org/win-x86-commandline/v4.1.0/nuget.exe"
-.\nuget restore -source "https://api.nuget.org/v3/index.json;https://www.myget.org/F/bramborman/api/v3/index.json"
+nuget restore -source "https://api.nuget.org/v3/index.json;https://www.myget.org/F/bramborman/api/v3/index.json"
 
 # Build
 $uwpHelperProjectFolder = Get-ChildItem -Directory -Filter "UWPHelper"
