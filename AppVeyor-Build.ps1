0b986240abd21ff6634037309b451d5ca422d5e5
Marian Dolinský <mariandolinsky@outlook.com>
2017-09-30 18:33:05 +0200

    Testing another way of adding NuGet sources

--- AppVeyor-Build.ps1
+++ AppVeyor-Build.ps1
@@ -8,8 +8,6 @@ Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildSc
 Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Set-PureBuildVersion.ps1"
 .\Set-PureBuildVersion.ps1
 
-nuget restore -source "https://api.nuget.org/v3/index.json;https://www.myget.org/F/bramborman/api/v3/index.json"
-
 # Build
 $uwpHelperProjectFolder = Get-ChildItem -Directory -Filter "UWPHelper"
 
